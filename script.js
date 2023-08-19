ebd8c5c161119757f86df66c53bfac202b2e44f7
pappukr3631 <100488442+pappukr3631@users.noreply.github.com>
2023-08-19 22:24:28 +0530

    commit

--- script.js
+++ script.js
@@ -40,7 +40,7 @@ function updateMap() {
     // "https://maps.google.com/maps?q=35.856737, 10.606619&z=15&output=embed"
     const map = document.getElementsByTagName("iframe")[0];
     map.src = `https://maps.google.com/maps?q=${latitude}, ${longitude}&x=15&output=embed`;
-    // console.log(map);
+
     postOfficeDetails();
 }
 
@@ -50,31 +50,45 @@ async function postOfficeDetails() {
     let result = await response.json();
 
     let details = result[0];
-    // console.log(result[0]);
-    console.log(details.PostOffice[0]);
+    // console.log(details.PostOffice[0]);
+    console.log(details);
+    console.log(result);
+
+    updateMoreInfo(details);
+
     
     displayPostOffice(details.PostOffice);
 }
 
+
+function updateMoreInfo(details) {
+    
+    // let chicago_datetime_str = new Date().toLocaleString("en-US", { timeZone: "America/Chicago" });
+    let timeZone = new Date().toLocaleString("en-US", { timeZone: "America/Chicago" });
+    let pin = document.getElementById("pinCode");
+    pin.innerText += pincode;
+    let msg = document.getElementById("message");
+    msg.innerText += details.Message;
+}
+
 function displayPostOffice(postOfficeArr) {
-    console.log("dia");
     let container = document.getElementById("postOfficeList");
     let innerContent = "";
     let n = postOfficeArr.length;
+
     for(let i=0; i<n; i++) {
         let postoffice = postOfficeArr[i];
         let content = `<div class="postOffice-card">
-        <p>Name ${postoffice.Name}</p>
-        <p>Branch Type ${postoffice.BranchType}</p>
-        <p>Delivery Status ${postoffice.DeliveryStatus}</p>
-        <p>District ${postoffice.District}</p>
-        <p>Division ${postoffice.Division}</p>
-    </div>`
+            <p>Name ${postoffice.Name}</p>
+            <p>Branch Type ${postoffice.BranchType}</p>
+            <p>Delivery Status ${postoffice.DeliveryStatus}</p>
+            <p>District ${postoffice.District}</p>
+            <p>Division ${postoffice.Division}</p>
+        </div>`
         // console.log(content);
         innerContent += content;
         // container.append(content);
     }
-    console.log("dia");
 
     container.innerHTML = innerContent;
 }
